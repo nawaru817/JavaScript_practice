5f8b65b3b77e2514ef923eb2755554a82ad6b50d
NawaruHarada <t010mbharada@gmail.com>
2021-05-31 23:52:48 +0900

    [Update] JS　関数練習

---
+++ app_function.js
@@ -0,0 +1,12 @@
+//メイン
+var alertString;
+alertString = addString("WebCamp");
+
+//関数呼び出し
+alert(alertString);
+
+//関数
+function addString(strA){
+  var addStr = "Hello " + strA;
+  return addStr;
+}
